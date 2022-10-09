468064635954db66a71406b0b4c019b62948a9ab
Gustavo Rodrigues <gustavomrodrigues11@gmail.com>
2022-10-09 16:02:55 -0300

    Finish Day_4

--- Day_4/RockPaperScissor.py
+++ Day_4/RockPaperScissor.py
@@ -1,55 +1,51 @@
 import random
-from secrets import choice
 
-# Rock
-rock = """
+rock = '''
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-"""
+'''
 
-# Paper
-paper = """
-     _______
----'    ____)____
-           ______)
+paper = '''
+    _______
+---'   ____)____
+          ______)
           _______)
          _______)
 ---.__________)
-"""
+'''
 
-# Scissors
-scissor = """
+scissors = '''
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-"""
+'''
 
-choice = int(input("select 0 rock, 1 paper, 2 scissor."))
+game_images = [rock, paper, scissors]
 
-player_choice = choice
+user_choice = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
 
-choice = random.randint(0,2)
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
 
-bot_choice = choice
-print(f"Bot choice = {bot_choice}")
-if player_choice > 0 or player_choice < 2:
-    print("You typed an invalid number, you lose!!")
-elif player_choice == 0 and bot_choice == 2:
-    print("You win!!")
-elif bot_choice == 0 and player_choice == 2:
-    print("You lose!!")
-elif bot_choice > player_choice:
-    print("You lose!!")
-elif player_choice > bot_choice:
-    print("You win!!")
-elif player_choice == bot_choice:
-    print("It's a Draw!!")
-else:
-    print("You type invalid number")
+if user_choice >= 3 or user_choice < 0:
+  print("You typed an invalid number, you lose!")
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
--- Day_4/resume.py
+++ Day_4/resume.py
@@ -1,3 +1,3 @@
 # Modules Random
-# Make a game Rock Paper Scissors
-# list
+# Make a game Rock Paper Scissors, Heads or Tails, Banker Roulette ( Russian Roulette) and Teasrure Map.
+# list in list
